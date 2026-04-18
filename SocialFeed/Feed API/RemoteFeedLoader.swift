baed389bc72198efd9053598bde063986766c1ce
Piyush <piyush@capitalnumbers.com>
2026-04-18 10:59:59 +0530

    Move `RemoteFeedLoader` component to production

---
+++ SocialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,23 @@
+//
+//  Created by CN23 on 18/04/26.
+//
+
+import Foundation
+
+
+public final class RemoteFeedLoader {
+  private let url: URL
+  private let client: HTTPClient
+  
+  public init(url: URL, client: HTTPClient) {
+    self.url =  url
+    self.client = client
+  }
+  public func load() {
+    client.get(from: url)
+  }
+}
+
+public protocol HTTPClient {
+  func get(from url: URL)
+}
