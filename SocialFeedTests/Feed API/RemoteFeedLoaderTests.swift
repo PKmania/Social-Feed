bd982ed2c98d67d1b7ace1eec6ece20496682ec5
Piyush <piyush@capitalnumbers.com>
2026-04-20 14:44:27 +0530

    Remove the generic error type from the `LoadFeedResult` to simplfy type constraints

--- SocialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ SocialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -38,7 +38,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   
   func test_load_deliversErrorOnClientError() {
     let (sut, client) = makeSUT()
-    expect(sut, toCompleteWith: .failure(.connectivity)) {
+    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity)) {
       let error = NSError(domain: "any", code: 0)
       client.complete(with: error)
     }
@@ -48,7 +48,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     let (sut, client) = makeSUT()
     let samples = [199, 201, 300, 400, 500]
     samples.enumerated().forEach { (index, code) in
-      expect(sut, toCompleteWith: .failure(.invalidData)) {
+      expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
         let json = makeItemsJSON([])
         client.complete(with: code, data: json , at: index)
       }
@@ -57,7 +57,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   
   func test_load_deliversErrorOnNon200HTTPResponseWithInvalidJSONData() {
     let (sut, client) = makeSUT()
-    expect(sut, toCompleteWith: .failure(.invalidData)) {
+    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
       let invalidJSON = Data()
       client.complete(with: 200, data: invalidJSON)
     }
@@ -141,7 +141,7 @@ class RemoteFeedLoaderTests: XCTestCase {
       switch (receivedResult, expectedResult) {
       case let (.success(receivedItems), .success(expectedItems)):
         XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-      case let (.failure(receivedError), .failure(expectedError)):
+      case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
         XCTAssertEqual(receivedError, expectedError, file: file, line: line)
         
       default:
