aa71ff65d5b643dbdbd4dc7ea181e7938d01133a
Piyush <piyush@capitalnumbers.com>
2026-04-20 06:56:48 +0530

    Move mapping logic to new type `FeedItemsMapper`

--- SocialFeed/Feed API/RemoteFeedLoader.swift
+++ SocialFeed/Feed API/RemoteFeedLoader.swift
@@ -47,18 +47,29 @@ public final class RemoteFeedLoader {
   }
 }
 
-
-private struct Root: Decodable {
-  let items: [Item]
-}
-
-private struct Item : Decodable{
-  private let id: UUID
-  private let description: String?
-  private let location: String?
-  private let image: URL
+private class FeedItemsMapper {
+  private struct Root: Decodable {
+    let items: [Item]
+  }
   
-  var item: FeedItem {
-    return FeedItem(id: id, description: description, location: location, imageURL: image)
+  private struct Item : Decodable{
+    private let id: UUID
+    private let description: String?
+    private let location: String?
+    private let image: URL
+    
+    var item: FeedItem {
+      return FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
+  }
+  
+  static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+    guard response .statusCode == 200 else {
+      throw RemoteFeedLoader.Error.invalidData
+    }
+    let root = try JSONDecoder().decode(Root.self, from: data)
+    return root.items.map { $0.item }
   }
 }
+
+
