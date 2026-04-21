5709ca6fc7257fc36274e28bf21f1b4f06061642
Piyush <piyush@capitalnumbers.com>
2026-04-21 14:51:36 +0530

    Move to `URLSessionHTTPClient` to its own file

---
+++ SocialFeed/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,26 @@
+//
+//  Created by CN23 on 21/04/26.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient{
+  private let session: URLSession
+  
+  public init (session: URLSession = .shared) {
+    self.session = session
+  }
+  private struct UnexpectedError: Error {}
+  
+  public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    session.dataTask(with: url) { (data, response, error) in
+      if let error = error {
+        completion(.failure(error))
+      }else if let data = data, let response = response as? HTTPURLResponse {
+        completion(.success((data, response)))
+      } else {
+        completion(.failure(UnexpectedError()))
+      }
+    }.resume()
+  }
+}
