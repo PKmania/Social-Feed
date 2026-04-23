c155eec742e821ed0853acc3309185d4e57eb03f
Piyush <piyush@capitalnumbers.com>
2026-04-23 15:02:23 +0530

    Move `LocalFeedLoader`  and `FeedStore` collablator to production code

---
+++ SocialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,12 @@
+//
+//  Created by CN23 on 23/04/26.
+//
+
+import Foundation
+
+public protocol FeedStore {
+  typealias DeletionCompletion = (Error?) -> Void
+  typealias InsertionCompletion = (Error?) -> Void
+  func deleteCachedFeed(completion: @escaping DeletionCompletion)
+  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
