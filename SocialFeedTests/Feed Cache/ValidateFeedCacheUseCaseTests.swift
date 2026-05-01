7107ffe86576484bd81ce9ae4d3372ecf4e37406
Piyush <piyush@capitalnumbers.com>
2026-05-02 00:49:35 +0530

    Seprate Date extension helpers into into  distinct contexts to clarify scope

--- SocialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ SocialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -53,7 +53,7 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
   func test_validateCache_deleteExpiredCache() {
     let feed = uniqueImageFeed()
     let fixedCurrentDate = Date()
-    let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(days: -1)
+    let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
     let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
     sut.validateCache()
     store.completeRetrival(with: feed.local, timestamp: expiredTimestamp)
