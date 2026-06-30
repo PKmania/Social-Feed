645f31add242f34a5b95c17793266f7dfd6a99f7
Piyush <piyush@capitalnumbers.com>
2026-06-30 17:07:48 +0530

    `CoreDataFeedStore.retrieve()` delivers found values on non-empty cache

--- SocialFeed/Feed Cache/Imfrastructure/ManagedCache.swift
+++ SocialFeed/Feed Cache/Imfrastructure/ManagedCache.swift
@@ -4,15 +4,4 @@
 
 import CoreData
 
-private class ManagedCache: NSManagedObject {
-  @NSManaged var timestamp: Date
-  @NSManaged var feed: NSOrderedSet
-}
 
-private class ManagedFeedImage: NSManagedObject {
-  @NSManaged var id: UUID
-  @NSManaged var imageDescription: String?
-  @NSManaged var location: String?
-  @NSManaged var url: URL
-  @NSManaged var cache: ManagedCache
-}
