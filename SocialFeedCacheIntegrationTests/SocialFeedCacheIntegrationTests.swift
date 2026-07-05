7a72ee11c23f2aef2ed798b36b2ee037200bf94b
Piyush <piyush@capitalnumbers.com>
2026-07-05 13:14:47 +0530

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` delivers items saved on separate instances, proving we correctly persist the data models to disk.

--- SocialFeedCacheIntegrationTests/SocialFeedCacheIntegrationTests.swift
+++ SocialFeedCacheIntegrationTests/SocialFeedCacheIntegrationTests.swift
@@ -38,6 +38,36 @@ final class SocialFeedCacheIntegrationTests: XCTestCase {
     wait(for: [exp], timeout: 1.0)
   }
   
+  func test_load_deliversItemsSavedOnASeparateInstance() {
+    let sutToPerformSave = makeSUT()
+    let sutToPerformLoad = makeSUT()
+    let feed = uniqueImageFeed().model
+    
+    let saveExp = expectation(description: "Wait for save completion")
+    sutToPerformSave.save(feed) { saveError in
+      XCTAssertNil(saveError, "Expected to save feed successfully")
+      saveExp.fulfill()
+    }
+    wait(for: [saveExp], timeout: 1.0)
+    
+    let loadExp = expectation(description: "Wait for load completion")
+    sutToPerformLoad.load { loadResult in
+      switch loadResult {
+      case let .success(imageFeed):
+        XCTAssertEqual(imageFeed, feed)
+        
+      case let .failure(error):
+        XCTFail("Expected successful feed result, got \(error) instead")
+        
+      default:
+          XCTFail("Received an unknown result.")
+      }
+      
+      loadExp.fulfill()
+    }
+    wait(for: [loadExp], timeout: 1.0)
+  }
+  
   // MARK: Helpers
 
   private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
