e239a4b9c4f5dba43c10b8a9c73b60bf116183cd
Piyush <piyush@capitalnumbers.com>
2026-07-10 23:57:54 +0530

    Load feed on viewDidLoad

---
+++ SocialFeedIOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,49 @@
+//
+//  Created by CN23 on 10/07/26.
+//
+
+import XCTest
+import UIKit
+
+class FeedViewController: UIViewController {
+  private var loader: FeedViewControllerTests.LoaderSpy?
+  
+  convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+    self.init()
+    self.loader = loader
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loader?.load()
+  }
+}
+
+final class FeedViewControllerTests: XCTestCase {
+  
+  func test_init_doesNotLoadFeed() {
+    let loader = LoaderSpy()
+    let _ = FeedViewController(loader: loader)
+    
+    XCTAssertEqual(loader.loadCallCount, 0)
+  }
+  
+  func test_viewDidLoad_loadFeed() {
+    let loader = LoaderSpy()
+    let sut = FeedViewController(loader: loader)
+    
+    sut.loadViewIfNeeded()
+    
+    XCTAssertEqual(loader.loadCallCount, 1)
+  }
+  
+  
+  
+  class LoaderSpy {
+    private(set) var loadCallCount = 0
+    
+    func load() {
+      loadCallCount += 1
+    }
+  }
+}
