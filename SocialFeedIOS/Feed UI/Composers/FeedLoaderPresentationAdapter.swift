0fa7fa32cc2b90ee7f6365370414eb09c8ddd84e
Piyush <piyush@capitalnumbers.com>
2026-09-10 09:50:49 +0530

    Move `FeedLoaderPresentationAdapter` to separate file

---
+++ SocialFeedIOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,25 @@
+//
+//  Created by CN23 on 10/09/26.
+//
+
+import Foundation
+import SocialFeed
+
+final class FeedLoaderPresentationAdaptor: FeedViewControllerDelegate {
+  private let feedLoader: FeedLoader
+  var presenter: FeedPresenter?
+  init(feedLoader: FeedLoader) {
+    self.feedLoader = feedLoader
+  }
+  func didRequestFeedRefresh() {
+    presenter?.didStartFeedLoading()
+    feedLoader.load { [weak self] (result) in
+      switch result {
+      case let .success(feed):
+        self?.presenter?.didFinishLoadingFeed(with: feed)
+      case let .failure(error):
+        self?.presenter?.didFinishLoadingFeed(with: error)
+      }
+    }
+  }
+}
--- SocialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
+++ SocialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
@@ -50,23 +50,6 @@ private final class FeedViewAdapter: FeedView {
 }
 
 
-private final class FeedLoaderPresentationAdaptor: FeedViewControllerDelegate {
-  private let feedLoader: FeedLoader
-  var presenter: FeedPresenter?
-  init(feedLoader: FeedLoader) {
-    self.feedLoader = feedLoader
-  }
-  func didRequestFeedRefresh() {
-    presenter?.didStartFeedLoading()
-    feedLoader.load { [weak self] (result) in
-      switch result {
-      case let .success(feed):
-        self?.presenter?.didFinishLoadingFeed(with: feed)
-      case let .failure(error):
-        self?.presenter?.didFinishLoadingFeed(with: error)
-      }
-    }
-  }
-}
+
 
 
