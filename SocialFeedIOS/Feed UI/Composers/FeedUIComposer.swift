d9fe09a276fe7baa57c9a2584d15b437a097dd3c
Piyush <piyush@capitalnumbers.com>
2026-09-10 09:52:55 +0530

    Move `FeedViewAdapter` to separate file

--- SocialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
+++ SocialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
@@ -29,25 +29,6 @@ private extension FeedViewController {
 }
 
 
-private final class FeedViewAdapter: FeedView {
-  private weak var controller: FeedViewController?
-  private var imageLoader: FeedImageDataLoader
-  
-  init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
-    self.controller = controller
-    self.imageLoader = imageLoader
-  }
-  func display(_ viewModel: FeedViewModel) {
-    controller?.tableModel = viewModel.feed.map({ model in
-      let adapter = FeedImageDataLoaderPresentationAdaptor<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
-      let view = FeedImageCellController(delegate: adapter)
-      
-      adapter.presenter = FeedImagePresenter(view: WeakRefVirtualProxy(view), imageTransformer: UIImage.init)
-      return view
-    })
-
-  }
-}
 
 
 
---
+++ SocialFeedIOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,25 @@
+//
+//  Created by CN23 on 10/09/26.
+//
+
+import Foundation
+import UIKit
+
+final class FeedViewAdapter: FeedView {
+  private weak var controller: FeedViewController?
+  private var imageLoader: FeedImageDataLoader
+  
+  init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+    self.controller = controller
+    self.imageLoader = imageLoader
+  }
+  func display(_ viewModel: FeedViewModel) {
+    controller?.tableModel = viewModel.feed.map({ model in
+      let adapter = FeedImageDataLoaderPresentationAdaptor<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
+      let view = FeedImageCellController(delegate: adapter)
+      
+      adapter.presenter = FeedImagePresenter(view: WeakRefVirtualProxy(view), imageTransformer: UIImage.init)
+      return view
+    })
+  }
+}
