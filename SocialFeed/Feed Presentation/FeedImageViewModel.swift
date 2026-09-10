e9f2a70f97309df4f78ae12e4162c48cadfbad46
Piyush <piyush@capitalnumbers.com>
2026-09-10 19:48:58 +0530

    Move `FeedImageViewModel` to separate file

---
+++ SocialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  Created by CN23 on 10/09/26.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+  public let description: String?
+  public let location: String?
+  public let image: Image?
+  public let isLoading: Bool
+  public let shouldRetry: Bool
+  
+  public var hasLocation: Bool {
+    return location != nil
+  }
+}
