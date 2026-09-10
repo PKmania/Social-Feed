4d4e63867adb55bb30a1d6d39c282a5f9201d1d7
Piyush <piyush@capitalnumbers.com>
2026-09-10 09:49:23 +0530

    Move `FeedImageDataLoaderPresentationAdapter` to separate file

--- SocialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
+++ SocialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
@@ -69,33 +69,4 @@ private final class FeedLoaderPresentationAdaptor: FeedViewControllerDelegate {
   }
 }
 
-private final class FeedImageDataLoaderPresentationAdaptor<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-  private var task: FeedImageDataLoaderTask?
-   private let model: FeedImage
-   private let imageLoader: FeedImageDataLoader
-  
-  init(model: FeedImage, imageLoader: FeedImageDataLoader) {
-    self.model = model
-    self.imageLoader = imageLoader
-  }
-  
-  var presenter: FeedImagePresenter<View, Image>?
-  func didRequestImage() {
-    let model = self.model
-    presenter?.didStartLoadingImageData(for: model)
-    
-    task = imageLoader.loadImageData(from: model.url, completion: { [weak self] (result) in
-      switch result {
-      case let .success(data):
-        self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-      case let .failure(error):
-        self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-      }
-    })
-  }
-  
-  
-  func didCancelImageRequest() {
-    task?.cancel()
-  }
-}
+
