6273cea8c960915f018543bc64efd3d6ef144512
Piyush <piyush@capitalnumbers.com>
2026-09-14 11:54:24 +0530

    `LocalFeedImageDataLoader.saveImageDataForURL` requests image data insertion for url into the store

--- SocialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ SocialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -69,6 +69,16 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
     XCTAssertTrue(received.isEmpty, "Expected no received results after instance has been deallocated")
   }
   
+  func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
+    let (sut, store) = makeSUT()
+    let url = anyURL()
+    let data = anyData()
+    
+    sut.save(data, for: url) { _ in }
+    
+    XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
+  }
+  
   // MARK: - Helpers
   
   private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
@@ -117,6 +127,7 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
   private class StoreSpy: FeedImageDataStore {
     enum Message: Equatable {
       case retrieve(dataFor: URL)
+      case insert(data: Data, for: URL)
     }
     private var completions = [(FeedImageDataStore.Result) -> Void]()
     private(set) var receivedMessages = [Message]()
@@ -133,6 +144,9 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
       completions[index](.success(data))
     }
     
+    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+          receivedMessages.append(.insert(data: data, for: url))
+        }
   }
   
 }
