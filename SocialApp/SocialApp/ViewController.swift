79056ed595db229d86259184ec87198fd205b773
Piyush <piyush@capitalnumbers.com>
2026-09-14 18:13:21 +0530

    Add empty SocialApp project

---
+++ SocialApp/SocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by CN23 on 14/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
