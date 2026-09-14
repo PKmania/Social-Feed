fd469c52ce600d21fcb63c6802ad83f2e0f00df2
Piyush <piyush@capitalnumbers.com>
2026-09-14 12:09:01 +0530

    Rename test case class to clarify intent

--- SocialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ SocialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -6,7 +6,7 @@ import Foundation
 import SocialFeed
 import XCTest
 
-class LocalFeedImageDataLoaderTests: XCTestCase {
+class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
   
   func test_init_doesNotMessageStoreUponCreation() {
     let (_, store) = makeSUT()
