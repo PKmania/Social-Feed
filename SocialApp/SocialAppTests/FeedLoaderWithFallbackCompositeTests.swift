fd303190d4f3c3819cc539f8383bff98b931c30d
Piyush <piyush@capitalnumbers.com>
2026-09-14 23:29:19 +0530

    Move `FeedLoaderWithFallbackComposite` to production

--- SocialApp/SocialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ SocialApp/SocialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -4,29 +4,8 @@
 
 import Foundation
 import XCTest
+import SocialApp
 import SocialFeed
-
-class FeedLoaderWithFallbackComposite: FeedLoader {
-  private let primary: FeedLoader
-  private let fallback: FeedLoader
-  
-  init(primary: FeedLoader, fallback: FeedLoader) {
-    self.primary = primary
-    self.fallback = fallback
-  }
-  
-  func load(completion: @escaping (FeedLoader.Result) -> Void) {
-    primary.load { [weak self] result in
-      switch result {
-      case .success:
-        completion(result)
-        
-      case .failure:
-        self?.fallback.load(completion: completion)
-      }
-    }
-  }
-}
 
 class FeedLoaderWithFallbackCompositeTests: XCTestCase {
   
