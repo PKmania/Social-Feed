c8c1d113e4639565d92876e2619805e26d8ab82e
Piyush <piyush@capitalnumbers.com>
2026-09-15 13:02:53 +0530

    Extract `FeedLoaderStub` into a shared scope to remove duplication

---
+++ SocialApp/SocialAppTests/Helpers/FeedLoaderStub.swift
@@ -0,0 +1,18 @@
+//
+//  Created by CN23 on 15/09/26.
+//
+
+import Foundation
+import SocialFeed
+
+class FeedLoaderStub: FeedLoader {
+  private let result: FeedLoader.Result
+  
+  init(result: FeedLoader.Result) {
+    self.result = result
+  }
+  
+  func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    completion(result)
+  }
+}
