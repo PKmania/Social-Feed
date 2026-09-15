11d98cf492cbdb6ed092354fd7947becc05bf78c
Piyush <piyush@capitalnumbers.com>
2026-09-15 13:23:55 +0530

    Move `FeedLoaderCacheDecorator` to production

---
+++ SocialApp/SocialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,25 @@
+//
+//  Created by CN23 on 15/09/26.
+//
+
+import Foundation
+import SocialFeed
+
+public final class FeedLoaderCacheDecorator: FeedLoader {
+  private let decoratee: FeedLoader
+  private let cache: FeedCache
+  
+  public init(decoratee: FeedLoader, cache: FeedCache) {
+    self.decoratee = decoratee
+    self.cache = cache
+  }
+  
+  public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    decoratee.load { [weak self] result in
+      completion(result.map({ feed in
+        self?.cache.save(feed) { _ in }
+        return feed
+      }))
+    }
+  }
+}
--- SocialApp/SocialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ SocialApp/SocialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -3,28 +3,9 @@
 //
 
 import Foundation
-
 import XCTest
 import SocialFeed
-
-
-final class FeedLoaderCacheDecorator: FeedLoader {
-  private let decoratee: FeedLoader
-  private let cache: FeedCache
-  init(decoratee: FeedLoader, cache: FeedCache) {
-    self.decoratee = decoratee
-    self.cache = cache
-  }
-  
-  func load(completion: @escaping (FeedLoader.Result) -> Void) {
-    decoratee.load { [weak self] result in
-      completion(result.map({ feed in
-        self?.cache.save(feed) { _ in }
-        return feed
-      }))
-    }
-  }
-}
+import SocialApp
 
 class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
   
