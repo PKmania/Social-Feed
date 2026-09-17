3e98599a005a1e255c5261dc4746954dec5308c1
Piyush <piyush@capitalnumbers.com>
2026-09-17 13:13:31 +0530

    Add UI Test target for running high-level Acceptance Tests

---
+++ SocialApp/SocialAppUITests/SocialAppUIAcceptanceTests.swift
@@ -0,0 +1,16 @@
+//
+//  Created by CN23 on 17/09/26.
+//
+
+import Foundation
+
+import XCTest
+
+class SocialAppUIAcceptanceTests: XCTestCase {
+  
+  func test_onLaunch() {
+    XCTAssertEqual("asd", "asd")
+  }
+  
+  
+}
