8bdf231d0ecaaa5b597a43b9aa2955ebd1e7016f
Piyush <piyush@capitalnumbers.com>
2026-09-21 12:28:45 +0530

    check CI

--- SocialFeedIOS/Feed UI/Composers/FeedViewAdapter.swift
+++ SocialFeedIOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -15,12 +15,12 @@ final class FeedViewAdapter: FeedView {
     self.imageLoader = imageLoader
   }
   func display(_ viewModel: FeedViewModel) {
-    controller?.tableModel = viewModel.feed.map({ model in
+    controller?.display(viewModel.feed.map({ model in
       let adapter = FeedImageDataLoaderPresentationAdaptor<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
       let view = FeedImageCellController(delegate: adapter)
       
       adapter.presenter = FeedImagePresenter(view: WeakRefVirtualProxy(view), imageTransformer: UIImage.init)
       return view
-    })
+    }))
   }
 }
