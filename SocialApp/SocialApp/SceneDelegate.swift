3c7d963b34ee81b6ed769dd01eccef8b7e665cfc
Piyush <piyush@capitalnumbers.com>
2026-09-21 19:40:49 +0530

    Improve a test name Remove unnecessary Main storyboard - the initial set up happens in code

--- SocialApp/SocialApp/SceneDelegate.swift
+++ SocialApp/SocialApp/SceneDelegate.swift
@@ -35,7 +35,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     
-    guard let _ = (scene as? UIWindowScene) else { return }
+    guard let scene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: scene)
     configureWindow()
   }
   
@@ -71,6 +72,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
               cache: localImageLoader)))
    let navVC =  UINavigationController(rootViewController: feedViewController)
     window?.rootViewController = navVC
+    window?.makeKeyAndVisible()
   }
   
   func sceneDidDisconnect(_ scene: UIScene) {
