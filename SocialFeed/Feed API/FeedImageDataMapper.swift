b19253e6ed0e418774c3a27956cf79b41d70b98e
Piyush <piyush@capitalnumbers.com>
2026-09-26 01:07:21 +0530

    Test FeedImageDataMapper in isolation

---
+++ SocialFeed/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,19 @@
+//
+//  Created by CN23 on 26/09/26.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+
+        return data
+    }
+}
