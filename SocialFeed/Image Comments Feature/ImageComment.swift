f92704c09df461585681483d92c0c9245a468145
Piyush <piyush@capitalnumbers.com>
2026-09-25 14:42:54 +0530

    Add ImageComment data model

---
+++ SocialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,19 @@
+//
+//  Created by CN23 on 25/09/26.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
