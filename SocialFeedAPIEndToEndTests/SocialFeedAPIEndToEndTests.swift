1985f24beecd9784dca03f64680a64c58c70eaed
Piyush <piyush@capitalnumbers.com>
2026-09-26 01:11:47 +0530

    Replace RemoteFeedImageDataLoader with HTTPClient publisher composed with FeedImageDataMapper

--- SocialFeedAPIEndToEndTests/SocialFeedAPIEndToEndTests.swift
+++ SocialFeedAPIEndToEndTests/SocialFeedAPIEndToEndTests.swift
@@ -6,7 +6,7 @@ import XCTest
 import SocialFeed
 
 final class SocialFeedAPIEndToEndTests: XCTestCase {
-
+  
   func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
     
     switch getFeedResult() {
@@ -20,14 +20,14 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
       XCTAssertEqual(imageFeed[5], expectedImage(at: 5))
       XCTAssertEqual(imageFeed[6], expectedImage(at: 6))
       XCTAssertEqual(imageFeed[7], expectedImage(at: 7))
-
+      
     case let .failure(error)?:
       XCTFail("Expected successful feed result, got \(error) instead")
-
+      
     default:
       XCTFail("Expected successful feed result, got no result instead")
     }
-
+    
   }
   
   func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
@@ -44,17 +44,21 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
   }
   
   // MARK: - Helpers
-
+  
   private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
+    let client = ephemeralClient()
     let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
-    let loader = RemoteFeedImageDataLoader(client: ephemeralClient())
-    trackMemoryLeaks(loader, file: file, line: line)
-    
     let exp = expectation(description: "Wait for load completion")
     
     var receivedResult: FeedImageDataLoader.Result?
-    _ = loader.loadImageData(from: url) { result in
-      receivedResult = result
+    client.get(from: url) { result in
+      receivedResult = result.flatMap { (data, response) in
+        do {
+          return .success(try FeedImageDataMapper.map(data, from: response))
+        } catch {
+          return .failure(error)
+        }
+      }
       exp.fulfill()
     }
     wait(for: [exp], timeout: 5.0)
@@ -64,9 +68,9 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
   
   private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> Swift.Result<[FeedImage], Error>? {
     let client = ephemeralClient()
-
+    
     let exp = expectation(description: "Wait for load completion")
-
+    
     var receivedResult: Swift.Result<[FeedImage], Error>?
     
     client.get(from: feedTestServerURL) { result in
@@ -79,21 +83,21 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
       }
       exp.fulfill()
     }
-
+    
     wait(for: [exp], timeout: 5.0)
-
+    
     return receivedResult
   }
   
   private var feedTestServerURL: URL {
-      return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-    }
+    return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+  }
   
   private func ephemeralClient(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
-      let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-      trackMemoryLeaks(client, file: file, line: line)
-      return client
-    }
+    let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    trackMemoryLeaks(client, file: file, line: line)
+    return client
+  }
   private func expectedImage(at index: Int) -> FeedImage {
     return FeedImage(
       id: id(at: index),
@@ -101,7 +105,7 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
       location: location(at: index),
       url: imageURL(at: index))
   }
-
+  
   private func id(at index: Int) -> UUID {
     return UUID(uuidString: [
       "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
@@ -114,7 +118,7 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
       "F79BD7F8-063F-46E2-8147-A67635C3BB01"
     ][index])!
   }
-
+  
   private func description(at index: Int) -> String? {
     return [
       "Description 1",
@@ -127,7 +131,7 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
       "Description 8"
     ][index]
   }
-
+  
   private func location(at index: Int) -> String? {
     return [
       "Location 1",
@@ -140,7 +144,7 @@ final class SocialFeedAPIEndToEndTests: XCTestCase {
       "Location 8"
     ][index]
   }
-
+  
   private func imageURL(at index: Int) -> URL {
     return URL(string: "https://url-\(index+1).com")!
   }
